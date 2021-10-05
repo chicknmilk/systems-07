230b43f10014a698f04b4865cf801f82851252f5
chicknmilk <aaronhsu4606@gmail.com>
2021-10-05 08:59:37 -0400

    function headers

--- main.c
+++ main.c
@@ -1,6 +1,28 @@
 #include <stdio.h>
 
+void print_array(int *arr, int size);
+float average_array(int *arr, int size);
+void copy_array(int *arr1, int *arr2, int size);
+
 int main() {
+    
+    int arr[5];
+    int i = 0;
+    for (i = 0; i < 5; i++) {
+        arr[i] = i;
+    }
+
+    int arr2[5];
+
+    print_array(arr, 5);
+
+    copy_array(arr, arr2, 5);
+
+    print_array(arr2, 5);
+
+    float b = average_array(arr, 5);
+
+    printf("%f\n", b);
 
     return 0;
 }
